003186eaecb9bec24e9d3ae5e275ca15acf01601
mohkhald <mohkhald@student.1337.ma>
2025-05-09 21:17:56 +0100

    add library's to .h file

---
+++ minishell.h
@@ -0,0 +1,17 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <errno.h>
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sysexits.h>
+# include <unistd.h>
+
+#endif
